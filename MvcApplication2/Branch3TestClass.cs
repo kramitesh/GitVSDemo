3d2df149f668f36eb35bce0064779ecc2c88e197
kramitesh <amitesh.a.kumar@capgemini.com>
2015-11-17 21:28:05 +0530

    branch3 test class added

---
+++ MvcApplication2/Branch3TestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2
+{
+    public class Branch3TestClass : Controller
+    {
+        //
+        // GET: /Branch3TestClass/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
